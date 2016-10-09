0c0127c278c9d231f6fb4f0b160d8efe89b56694
Danilo Horta <danilo.horta@gmail.com>
2016-10-09 21:32:06 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,52 @@
+import os
+import sys
+from setuptools import setup
+from setuptools import find_packages
+
+
+def setup_package():
+    src_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+    old_path = os.getcwd()
+    os.chdir(src_path)
+    sys.path.insert(0, src_path)
+
+    needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+    pytest_runner = ['pytest-runner'] if needs_pytest else []
+
+    setup_requires = [] + pytest_runner
+    install_requires = ['pytest', 'scipy>=0.17', 'numpy>=1.9', 'nose>=1.3',
+                        'six', 'ndarray_listener>=1.0']
+    tests_require = install_requires
+
+    metadata = dict(
+        name='optimix',
+        version='1.0.0.dev1',
+        maintainer="Danilo Horta",
+        maintainer_email="horta@ebi.ac.uk",
+        license="BSD",
+        url='https://github.com/Horta/optimix',
+        packages=find_packages(),
+        zip_safe=True,
+        install_requires=install_requires,
+        setup_requires=setup_requires,
+        tests_require=tests_require,
+        include_package_data=True,
+    )
+
+    try:
+        from distutils.command.bdist_conda import CondaDistribution
+    except ImportError:
+        pass
+    else:
+        metadata['distclass'] = CondaDistribution
+        metadata['conda_buildnum'] = 1
+        metadata['conda_features'] = ['mkl']
+
+    try:
+        setup(**metadata)
+    finally:
+        del sys.path[0]
+        os.chdir(old_path)
+
+if __name__ == '__main__':
+    setup_package()
