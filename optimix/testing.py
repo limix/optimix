0e8f321fb7c84b4bb24cb05674d0155b0a235ef8
Danilo Horta <danilo.horta@gmail.com>
2017-01-31 00:11:18 +0000

    automatic

--- optimix/testing.py
+++ optimix/testing.py
@@ -14,15 +14,15 @@ class Assertion(object):
         self._item1 = item1
         self._value_example = value_example
 
-    def isitem(self, v):
+    def _isitem(self, v):
         e = self._value_example
         return isinstance(v, type(e))
 
-    def isvector(self, v):
-        return not self.isitem(v) and self.isitem(_ni(v))
+    def _isvector(self, v):
+        return not self._isitem(v) and self._isitem(_ni(v))
 
-    def ismatrix(self, v):
-        return not self.isitem(v) and not self.isvector(v) and _ni(_ni(v))
+    def _ismatrix(self, v):
+        return not self._isitem(v) and not self._isvector(v) and _ni(_ni(v))
 
     def assert_value_shape(self):
 
@@ -38,21 +38,37 @@ class Assertion(object):
         for cx in containers:
             for cy in containers:
                 f = func()
-                self.assert_shape(f.value(cx[0], cy[0]), cx[1], cy[1])
+                self._assert_shape(f.value(cx[0], cy[0]), cx[1], cy[1])
 
-    def assert_shape(self, value, cx, cy):
+    # def assert_gradient_shape(self):
+    #
+    #     func = self._func
+    #     item0 = self._item0
+    #     item1 = self._item1
+    #
+    #     containers = [(item0, "item"), ([item0], "vector"),
+    #                   (_compact([item0]), "vector"),
+    #                   ([item0, item1], "vector"),
+    #                   (_compact([item0, item1]), "vector")]
+    #
+    #     for cx in containers:
+    #         for cy in containers:
+    #             f = func()
+    #             self._assert_shape(f.value(cx[0], cy[0]), cx[1], cy[1])
+
+    def _assert_shape(self, value, cx, cy):
         def errmsg(premiss):
             return "Interface premiss %s violated." % premiss
 
         if cx == "item" and cy == "item":
-            if not self.isitem(value):
+            if not self._isitem(value):
                 raise AssertionError(errmsg("value(item, item) -> item"))
         elif cx == "item" and cy == "vector":
-            if not self.isvector(value):
+            if not self._isvector(value):
                 raise AssertionError(errmsg("value(item, vector) -> vector"))
         elif cx == "vector" and cy == "item":
-            if not self.isvector(value):
+            if not self._isvector(value):
                 raise AssertionError(errmsg("value(vector, item) -> vector"))
         elif cx == "vector" and cy == "vector":
-            if not self.ismatrix(value):
+            if not self._ismatrix(value):
                 raise AssertionError(errmsg("value(vector, vector) -> matrix"))
