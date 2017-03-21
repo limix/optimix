b709cdf9908ad981da146290f63449b1a114d342
Danilo Horta <danilo.horta@gmail.com>
2017-03-21 12:57:40 +0000

    passing

--- optimix/testing.py
+++ optimix/testing.py
@@ -23,6 +23,7 @@ def _do_flatten(x):
 
 
 def _isitem(v, e):
+    v = ndarray_listener(v)
     return isinstance(v, type(e)) and v.shape == e.shape
 
 
@@ -31,7 +32,8 @@ def _isvector(v, e):
 
 
 def _ismatrix(v, e):
-    return not _isitem(v, e) and not _isvector(v, e) and _ni(_ni(v))
+    return not _isitem(v, e) and not _isvector(v, e) and _isitem(
+        _ni(_ni(v)), e)
 
 
 class Assertion(object):
@@ -77,7 +79,7 @@ class Assertion(object):
         for cx in containers:
             for cy in containers:
                 f = func()
-                self._assert_valshape_msg(f.value(cx[0], cy[0]), cx[1], cy[1])
+                self._assert_valshape_msg(f.value(cx[0], cy[0]), cx, cy)
 
     def _assert_derivative_shape(self):
 
@@ -90,46 +92,56 @@ class Assertion(object):
                 f = func()
                 d = f.gradient(cx[0], cy[0])
                 for name in d.keys():
-                    self._assert_dershape_message(d[name], cx[1], cy[1], name)
+                    self._assert_dershape_message(d[name], cx, cy, name)
 
     def _assert_valshape_msg(self, value, cx, cy):
-        def errmsg(premiss):
-            return "Interface premiss %s violated." % premiss
 
-        if cx == "item" and cy == "item":
+        if cx[1] == "item" and cy[1] == "item":
             if not _isitem(value, self._value_example):
-                raise AssertionError(errmsg("value(item, item) -> item"))
-        elif cx == "item" and cy == "vector":
+                raise AssertionError(
+                    _errmsg("value(item, item) -> item", value, cx[0], cy[0]))
+        elif cx[1] == "item" and cy[1] == "vector":
             if not _isvector(value, self._value_example):
-                raise AssertionError(errmsg("value(item, vector) -> vector"))
-        elif cx == "vector" and cy == "item":
+                raise AssertionError(
+                    _errmsg("value(item, vector) -> vector", value, cx[0], cy[
+                        0]))
+        elif cx[1] == "vector" and cy[1] == "item":
             if not _isvector(value, self._value_example):
-                raise AssertionError(errmsg("value(vector, item) -> vector"))
-        elif cx == "vector" and cy == "vector":
+                raise AssertionError(
+                    _errmsg("value(vector, item) -> vector", value, cx[0], cy[
+                        0]))
+        elif cx[1] == "vector" and cy[1] == "vector":
             if not _ismatrix(value, self._value_example):
-                raise AssertionError(errmsg("value(vector, vector) -> matrix"))
+                raise AssertionError(
+                    _errmsg("value(vector, vector) -> matrix", value, cx[0],
+                            cy[0]))
 
-    def _assert_dershape_message(self, derivative, cx, cy, derivative_name):
-        def errmsg(premiss):
-            return "Interface premiss %s violated." % premiss
+    def _assert_dershape_message(self, der, cx, cy, der_name):
 
-        if cx == "item" and cy == "item":
-            if not _isitem(derivative,
-                           self._derivative_examples[derivative_name]):
+        if cx[1] == "item" and cy[1] == "item":
+            if not _isitem(der, self._derivative_examples[der_name]):
                 raise AssertionError(
-                    errmsg("%s(item, item) -> item" % derivative_name))
-        elif cx == "item" and cy == "vector":
-            if not _isvector(derivative,
-                             self._derivative_examples[derivative_name]):
+                    _errmsg("%s(item, item) -> item" % der_name, der, cx[0],
+                            cy[0]))
+        elif cx[1] == "item" and cy[1] == "vector":
+            if not _isvector(der, self._derivative_examples[der_name]):
                 raise AssertionError(
-                    errmsg("%s(item, vector) -> vector" % derivative_name))
-        elif cx == "vector" and cy == "item":
-            if not _isvector(derivative,
-                             self._derivative_examples[derivative_name]):
+                    _errmsg("%s(item, vector) -> vector" % der_name, der, cx[
+                        0], cy[0]))
+        elif cx[1] == "vector" and cy[1] == "item":
+            if not _isvector(der, self._derivative_examples[der_name]):
                 raise AssertionError(
-                    errmsg("%s(vector, item) -> vector" % derivative_name))
-        elif cx == "vector" and cy == "vector":
-            if not _ismatrix(derivative,
-                             self._derivative_examples[derivative_name]):
+                    _errmsg("%s(vector, item) -> vector" % der_name, der, cx[
+                        0], cy[0]))
+        elif cx[1] == "vector" and cy[1] == "vector":
+            if not _ismatrix(der, self._derivative_examples[der_name]):
                 raise AssertionError(
-                    errmsg("%s(vector, vector) -> matrix" % derivative_name))
+                    _errmsg("%s(vector, vector) -> matrix" % der_name, der, cx[
+                        0], cy[0]))
+
+
+def _errmsg(premiss, value, lval, rval):
+    msg = "Interface premiss %s violated." % premiss
+    msg += "\n  Got (%s:%s, %s:%s) -> %s:%s instead." % (
+        type(lval), lval, type(rval), rval, type(value), value)
+    return msg
