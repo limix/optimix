5bed709ec01f2c288f07271af6153c31faa2d8c2
Danilo Horta <danilo.horta@gmail.com>
2017-03-21 15:15:23 +0000

    brent working again

--- optimix/optimize/brent.py
+++ optimix/optimize/brent.py
@@ -12,8 +12,21 @@ class ProxyFunction(object):
         self._progress = tqdm(desc='Optimix', disable=not progress)
         self._iteration = 0
 
+    def names(self):
+        return sorted(self._function.variables().select(fixed=False).names())
+
     def value(self):
         return self._signal * self._function.value()
+
+    def unflatten(self, x):
+        variables = self._function.variables().select(fixed=False)
+        d = dict()
+        offset = 0
+        for name in self.names():
+            size = variables.get(name).size
+            d[name] = x[offset:offset + size]
+            offset += size
+        return d
 
     def __call__(self, x):
         self._progress.update(1)
@@ -21,12 +34,12 @@ class ProxyFunction(object):
         self._iteration += 1
 
         x = asarray(x).ravel()
-        self._function.variables().from_flat(x)
+        self._function.variables().set(self.unflatten(x))
 
         return self.value()
 
     def set_solution(self, x):
-        self._function.variables().from_flat(asarray(x).ravel())
+        self._function.variables().set(self.unflatten(x))
         self._progress.close()
 
 
@@ -35,15 +48,9 @@ def _minimize(proxy_function):
     proxy_function.set_solution(x)
 
 
-def minimize(function, progress=False):
-    t = function.variables().select(fixed=False)
-    if len(t) == 0:
-        return function.variables().flatten()[0]
+def minimize(function, progress=True):
     return _minimize(ProxyFunction(function, progress, False))
 
 
 def maximize(function, progress=False):
-    t = function.variables().select(fixed=False)
-    if len(t) == 0:
-        return function.variables().flatten()[0]
     return _minimize(ProxyFunction(function, progress, True))
