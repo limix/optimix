f21ce729b428a1f3dacea403f5793df258cb797c
Danilo Horta <danilo.horta@gmail.com>
2018-01-24 18:34:17 +0000

    setup

--- conftest.py
+++
@@ -1,9 +0,0 @@
-def get_pkg_name():
-    from setuptools import find_packages
-
-    return find_packages()[0]
-
-
-collect_ignore = [
-    "doc/conf.py", "setup.py", "{}/testit.py".format(get_pkg_name())
-]
--- optimix/__init__.py
+++ optimix/__init__.py
@@ -14,13 +14,10 @@ from .function import Function, FunctionReduce
 from .optimize import maximize, maximize_scalar, minimize, minimize_scalar
 from .types import Matrix, Scalar, Vector
 
-__name__ = "optimix"
-__version__ = "1.2.21"
-__author__ = "Danilo Horta"
-__author_email__ = "horta@ebi.ac.uk"
+__version__ = "1.2.22"
 
 __all__ = [
-    "__name__", "__version__", "__author__", "__author_email__", "test",
+    "__version__", "test",
     'testing', 'approx_fprime', 'check_grad', 'OptimixError', 'Function',
     'FunctionReduce', 'maximize', 'maximize_scalar', 'minimize',
     'minimize_scalar', 'Matrix', 'Scalar', 'Vector'
