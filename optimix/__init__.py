deeea2bbefd52aad92e0f15d66f4bead12190dbc
Danilo Horta <danilo.horta@gmail.com>
2018-01-25 11:22:47 +0000

    travis, setup, requirements

--- optimix/__init__.py
+++ optimix/__init__.py
@@ -6,19 +6,21 @@ optimix package
 Abstract function optimisation.
 
 """
+from __future__ import absolute_import as _
+
 from . import testing
-from .testit import test
 from .check_grad import approx_fprime, check_grad
 from .exception import OptimixError
 from .function import Function, FunctionReduce
 from .optimize import maximize, maximize_scalar, minimize, minimize_scalar
+from .testit import test
 from .types import Matrix, Scalar, Vector
 
 __version__ = "1.2.22"
 
 __all__ = [
-    "__version__", "test",
-    'testing', 'approx_fprime', 'check_grad', 'OptimixError', 'Function',
-    'FunctionReduce', 'maximize', 'maximize_scalar', 'minimize',
-    'minimize_scalar', 'Matrix', 'Scalar', 'Vector'
+    "__version__", "test", 'testing', 'approx_fprime', 'check_grad',
+    'OptimixError', 'Function', 'FunctionReduce', 'maximize',
+    'maximize_scalar', 'minimize', 'minimize_scalar', 'Matrix', 'Scalar',
+    'Vector'
 ]
