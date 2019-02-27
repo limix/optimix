7b915fae01fc33d86d7359d69f9ab48aca7259aa
Danilo Horta <danilo.horta@gmail.com>
2019-02-27 03:02:33 +0000

    del

--- optimix/_optimize/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Optimization methods.
-"""
-
-from ._bfgs import maximize, minimize
-from ._brent import maximize as maximize_scalar, minimize as minimize_scalar
-
-__all__ = ["minimize", "maximize", "minimize_scalar", "maximize_scalar"]
